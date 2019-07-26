d274eb62cfb2d363664fbc8382cd582acb4b7208
Xinyu Zhou <zxytim@gmail.com>
2019-07-27 00:53:33 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+with open("requirements.txt") as f:
+    requirements = [line.strip() for line in f]
+
+
+setuptools.setup(
+    name="shmarray",
+    version="0.0.1",
+    author="Xinyu Zhou",
+    author_email="zxytim@gmail.com",
+    description="A polished shmarray, originated by David Baddeley",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zxytim/shmarray",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
